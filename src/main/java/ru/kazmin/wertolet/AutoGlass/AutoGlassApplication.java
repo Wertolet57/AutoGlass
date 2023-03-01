d1b7d6035ec016dec4f63d80729c318762846591
Sergey Kazmin <kazmin5796@mail.ru>
2023-03-01 13:48:38 +0300

    Create maven project

---
+++ src/main/java/ru/kazmin/wertolet/AutoGlass/AutoGlassApplication.java
@@ -0,0 +1,13 @@
+package ru.kazmin.wertolet.AutoGlass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoGlassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoGlassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/kazmin/wertolet/AutoGlass/AutoGlassApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kazmin.wertolet.AutoGlass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoGlassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
